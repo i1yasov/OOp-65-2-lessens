6b382bb786a74ba83dca40e2bf61e2ed4ad49591
i1yasov <beka05274@gmail.com>
2026-03-29 23:30:11 +0600

    lesen1

---
+++ lesen1.py
@@ -0,0 +1,18 @@
+class Person:
+    #Констуктор класса
+    def __init__(self,name,hp,lvl):
+        #Атрибуты класса
+        self.name_Person = name
+        self.hp_Person = hp
+        self.lvl_Person = lvl
+        #Динамические переменные
+    def action(self):
+        return f'{self.name_Person} base action!!'
+
+
+
+
+#Обьект экземпляр на основе класса
+kirito = Person('kirito',1000,100)
+asuna = Person('asuna',10000,1000)
+print(kirito.hp_Person)
