ad04fdf5b8f6359245fbdbeab056dceea738883d
i1yasov <beka05274@gmail.com>
2026-04-14 14:30:42 +0600

    homework5

---
+++ Homework 2_month/homework5-2month.py
@@ -0,0 +1,7 @@
+from colorama import Fore, Style
+
+# Эта библиотека нужна для изменения цвета текста в консоли
+
+print(Fore.RED + "Ошибка!")
+print(Fore.GREEN + "Все работает!")
+print(Style.RESET_ALL)
--- lessen2_month/lesen3-2month.py
+++ lessen2_month/lesen3-2month.py
@@ -1,13 +1,17 @@
 class BankAccount:
-    def __init__(self,login,password):
+    def __init__(self,login,balance,password):
         self.login = login
-        self.password = passwords
+        self._balance = balance
+        self.password = password
+    def get_balance(self,password):
+        if password == self.password:
+            return self._balance
+        else:
+            return 'Неверный пороль '
 
 
 
-
-
-
-
-
-
+ardager = BankAccount(login="ardager",balance=10000,password="1111")
+print(ardager._balance)
+print(ardager.login)
+print(ardager.password)
