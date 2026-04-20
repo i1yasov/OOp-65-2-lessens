19840844b2bd4344b01f55b72ba4dbe876f5943d
i1yasov <beka05274@gmail.com>
2026-04-20 23:36:07 +0600

    obdeit

--- lessen2_month/lesen7-2month.py
+++ lessen2_month/lesen7-2month.py
@@ -1,23 +1,3 @@
-# import sqlite3
-# #A4
-# connect = sqlite3.connect('users.db')
-# # Рука с ручкой
-# cursor = connect.cursor()
-# cursor.execute('''
-#     CREATE TABLE  IF NOT EXISTS users(
-#         name VARCHAR (50) NOT NULL,
-#         age INTEGER NOT NULL,
-#         hobby TEXT
-# ''')
-# connect.commit()
-# # CRUD Create - READ -Update -Delete
-# def create_user(name, age, hobby):
-#     cursor.execute(f'INSERT INTO users (name, age, hoppy)   VALUES ("{name}", "{age}", "{hobby}")')
-#     connect.commit()
-#     cursor.execute(
-#         'INSERT INTO users(name, age, hobby) VALUES (?, ?, ?) ',
-#         (name, age, hobby)
-#     )
 import sqlite3
 
 # A4
@@ -27,6 +7,7 @@ cursor = connect.cursor()
 
 cursor.execute('''
         CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             name VARCHAR (50) NOT NULL,
             age INTEGER NOT NULL,
             hobby TEXT
@@ -46,5 +27,35 @@ def create_user(name, age, hobby):
     connect.commit()
     print('Пользователь добавлен!!')
 
+# create_user("Вася", 13, "Горы!!")
+# create_user("Вася", 13, "Горы!!")
+# create_user("Вася", 13, "Горы!!")
 
-create_user("John Doe", 33, "Горы!!")
+def read_users():
+    cursor.execute('SELECT name, hobby FROM users')
+    user = cursor.fetchmany(3)
+    print(f"all users\n {user}")
+
+# read_users()
+
+def update_user(new_name, rowid):
+    cursor.execute(
+        'UPDATE users SET name = ? WHERE rowid = ?',
+        (new_name, rowid)
+    )
+    connect.commit()
+    print("User updated!!")
+
+# update_user("John", 3)
+
+# read_users()
+
+def delete_user(id):
+    cursor.execute(
+        'DELETE FROM users WHERE id = ?',
+        (id,)
+    )
+    connect.commit()
+    print('Пользователь удален!!')
+
+# delete_user(2)
